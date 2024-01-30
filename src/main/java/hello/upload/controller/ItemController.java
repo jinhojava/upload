c1b9104af026d8247e206c0cef13bd16bcf9e33e
jinhojava <ho042479@gmail.com>
2024-01-30 11:34:09 +0900

    first commit2

--- src/main/java/hello/upload/controller/ItemController.java
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -68,11 +68,12 @@ public class ItemController {
         return "item-view";
     }
 
+    //실제 이미지 파일 url
     @ResponseBody
     @GetMapping("/images/{filename}")
     public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
        return new UrlResource("file:"+fileStore.getFullPath(filename));
-        //UrlResource가 파일경로 이름 주면 알아서 파일 찾아온다
+        //UrlResource가 파일경로와 이름을 주면 알아서 파일 찾아온다.
     }
 
     @GetMapping("/attach/{itemId}")//첨부파일다운
@@ -86,7 +87,7 @@ public class ItemController {
         //UrlResource가 파일경로 이름 주면 알아서 파일 찾아온다
         log.info("uploadFileName={}",uploadFileName);
 
-        //한글 파일명인 것들 깨짐 방지
+        //한글 파일명 파일 깨짐 방지
         String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
 
         String contentDisposition="attachment; filename=\""+encodedUploadFileName+"\"";
@@ -98,7 +99,7 @@ public class ItemController {
             }
 
 //    Content-Disposition 헤더생성코드. 여기서 attachment는 파일을 다운로드하도록지시하고, filename은 다운로드될 파일의 이름을 지정.
-//uploadFileName은 다운로드될 파일의 실제 이름을 나타냅니다. 이 이름은 클라이언트(브라우저)에서 다운로드할 때 사용. \와 "는 이중 따옴표를 문자열 안에 포함시키기 위한 이스케이프 문자.
+//uploadFileName은 다운로드될 파일의 실제 이름을 나타냄. 이 이름은 클라이언트(브라우저)에서 다운로드할 때 사용. \와 "는 이중 따옴표를 문자열 안에 포함시키기 위한 이스케이프 문자.
 //    따라서, 예를 들어 uploadFileName이 "example.txt"라면, 최종적으로 Content-Disposition 헤더는 다음과 같이 될 것.
 //    Content-Disposition: attachment; filename="example.txt"
 //    이렇게 설정된 헤더는 브라우저에게 해당 파일을 다운로드하라는 지시를 전달하고, 다운로드되는 파일의 이름을 "example.txt"로 표시.
--- src/main/java/hello/upload/controller/ItemForm.java
+++ src/main/java/hello/upload/controller/ItemForm.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-//상품저장용폼
+//상품저장용 폼
 @Data
 public class ItemForm {
 
@@ -12,7 +12,7 @@ public class ItemForm {
     private String itemName;
 
     //단일파일
-    private MultipartFile attachFile;//멀티파트는 @ModelAttribute에서 그대로 사용할 수 있다.
+    private MultipartFile attachFile;//참고 : @ModelAttribute에서 MultipartFile를 그대로 사용할 수 있다.
 
     //다중파일
     private List<MultipartFile> imageFiles;//이미지를 다중 업로드 하기위해 MultipartFile사용
